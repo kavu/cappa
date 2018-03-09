0e9b5bcac545f3389b8ecb2cc7aa010d2c05c9cb
Max Riveiro <kavu13@gmail.com>
2018-03-09 10:43:00 +0300

    Fix num lines
    Signed-off-by: Max Riveiro <kavu13@gmail.com>

--- test.bash
+++ test.bash
@@ -4,5 +4,5 @@ set -e
 
 wget "$DB_URL" -O "$TMP_PATH"
 sed -i '1,4d' "$TMP_PATH"
-sed '145500,146000d' "$TMP_PATH" | head -300 > "$DB_PATH"
+sed '145500,146000d' "$TMP_PATH" > "$DB_PATH"
 go test -v -cpu="$CPU" -benchmem -bench=.
