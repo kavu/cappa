27a7fcc040a3189fd2d0077b77bbe927753c05a9
Max Riveiro <kavu13@gmail.com>
2018-09-20 17:34:59 +0300

    Add more data to match on, disable benchmarks

--- test.bash
+++ test.bash
@@ -4,5 +4,6 @@ set -e
 
 wget "$DB_URL" -O "$TMP_PATH"
 sed -i '1,4d' "$TMP_PATH"
-grep "Chrome/56.0" "$TMP_PATH" > "$DB_PATH"
-go test -v -cpu="$CPU" -benchmem -bench=.
+grep "iPhone OS 12" "$TMP_PATH" > "$DB_PATH"
+grep "Chrome/56.0" "$TMP_PATH" >> "$DB_PATH"
+# go test -v -cpu="$CPU" -benchmem -bench=.
