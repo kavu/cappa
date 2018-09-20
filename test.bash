fe4801645c6fd37436ab8d59c25775bd13eced0b
Max Riveiro <kavu13@gmail.com>
2018-09-20 17:30:11 +0300

    Update test.bash to fix tests

--- test.bash
+++ test.bash
@@ -4,5 +4,5 @@ set -e
 
 wget "$DB_URL" -O "$TMP_PATH"
 sed -i '1,4d' "$TMP_PATH"
-sed -n '145500,146000p' "$TMP_PATH" > "$DB_PATH"
+grep "Chrome/56.0" "$TMP_PATH" > "$DB_PATH"
 go test -v -cpu="$CPU" -benchmem -bench=.
