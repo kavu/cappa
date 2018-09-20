b98a35a49de3b1b0f5ff93cc04cdecc1e34ed399
Max Riveiro <kavu13@gmail.com>
2018-09-20 18:00:37 +0300

    Update test.bash to fix tests (#5)
    * Update test.bash to fix tests
    * Add more data to match on, disable benchmarks

--- test.bash
+++ test.bash
@@ -4,5 +4,6 @@ set -e
 
 wget "$DB_URL" -O "$TMP_PATH"
 sed -i '1,4d' "$TMP_PATH"
-sed -n '145500,146000p' "$TMP_PATH" > "$DB_PATH"
-go test -v -cpu="$CPU" -benchmem -bench=.
+grep "iPhone OS 12" "$TMP_PATH" > "$DB_PATH"
+grep "Chrome/56.0" "$TMP_PATH" >> "$DB_PATH"
+# go test -v -cpu="$CPU" -benchmem -bench=.
