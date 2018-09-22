e088b37deb4c9b86f94c2af0a3f1ba4f50538ad2
Max Riveiro <kavu13@gmail.com>
2018-09-22 10:05:34 +0300

    Fix test running - actually tests need to be run

--- test.bash
+++ test.bash
@@ -6,4 +6,4 @@ wget "$DB_URL" -O "$TMP_PATH"
 sed -i '1,4d' "$TMP_PATH"
 grep "iPhone OS 12" "$TMP_PATH" > "$DB_PATH"
 grep "Chrome/56.0" "$TMP_PATH" >> "$DB_PATH"
-# go test -v -cpu="$CPU" -benchmem -bench=.
+go test -v -cpu="$CPU"
